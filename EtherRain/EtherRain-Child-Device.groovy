87763002b2fb39da6e9cd2ffab32a9586893778a
c steele <csteele@pandoradigital.com>
2023-04-23 10:46:24 -0700

    v1.0.2

--- EtherRain/EtherRain-Child-Device.groovy
+++ EtherRain/EtherRain-Child-Device.groovy
@@ -12,9 +12,10 @@
  *
  */
 
-public static String version()      {  return "v1.0.0"  }
+public static String version()      {  return "v1.0.2"  }
 
 /***********************************************************************************************************************
+ *         v1.0.2     removed standalone version check (allow HPM to check.)
  * Version: 1.0.0
  *
  */
@@ -44,7 +45,7 @@ void close() {
 
 void parse(String description) { log.warn "parse(String description) not implemented" }
 void parse(List description) { log.warn "parse(List description) not implemented" }
-void parse(Map evt) { sendEvent(evt) }
+void parse(Map evt) { sendEvent(evt) }  // report an Event in Parent, report Valve event in child.
 
 /*
 	updated
@@ -78,61 +79,8 @@ void installed()
 */
 void initialize()
 {
-	schedule("0 0 8 ? * FRI *", updateCheck)
-	runIn(20, updateCheck) 
+	unschedule()
 	log.trace "EtherRain Child Initialize"
-}
-
-
-// Check Version   ***** with great thanks and acknowledgment to Cobra (CobraVmax) for his original code ****
-void updateCheck()
-{    
-//	def paramsUD = [uri: "https://hubitatcommunity.github.io/EtherRainValves/version2.json"]
-	def paramsUD = [uri: "https://csteele-pd.github.io/Hubitat-master/version2.json"]
-	
- 	asynchttpGet("updateCheckHandler", paramsUD) 
-}
-
-void updateCheckHandler(resp, data) {
-
-	state.InternalName = "EtherRain Device (Child)"
-
-	if (resp.getStatus() == 200 || resp.getStatus() == 207) {
-		respUD = parseJson(resp.data)
-		// log.warn " Version Checking - Response Data: $respUD"   // Troubleshooting Debug Code - Uncommenting this line should show the JSON response from your webserver 
-		state.Copyright = "${thisCopyright}"
-		// uses reformattted 'version2.json' 
-		def newVer = (respUD.driver.(state.InternalName).ver.replaceAll("[.vV]", ""))
-		def currentVer = version().replaceAll("[.vV]", "")                
-		state.UpdateInfo = (respUD.driver.(state.InternalName).updated)
-            // log.debug "updateCheck: ${respUD.driver.(state.InternalName).ver}, $state.UpdateInfo, ${respUD.author}"
-
-		switch(newVer) {
-			case { it == "NLS"}:
-			      state.Status = "<b>** This Driver is no longer supported by ${respUD.author}  **</b>"       
-			      log.warn "** This Driver is no longer supported by ${respUD.author} **"      
-				break
-			case { it > currentVer}:
-			      state.Status = "<b>New Version Available (Version: ${respUD.driver.(state.InternalName).ver})</b>"
-			      log.warn "** There is a newer version of this Driver available  (Version: ${respUD.driver.(state.InternalName).ver}) **"
-			      log.warn "** $state.UpdateInfo **"
-				break
-			case { it < currentVer}:
-			      state.Status = "<b>You are using a Test version of this Driver (Expecting: ${respUD.driver.(state.InternalName).ver})</b>"
-				break
-			default:
-				state.Status = "Current"
-				if (descTextEnable) log.info "You are using the current version of this driver"
-				break
-		}
-
- 	sendEvent(name: "verUpdate", value: state.UpdateInfo)
-	sendEvent(name: "verStatus", value: state.Status)
-      }
-      else
-      {
-           log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI"
-      }
 }
 
 void getThisCopyright(){"&copy; 2019 C Steele "}
