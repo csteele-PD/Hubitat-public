458264a9b6da8682b1b51098c8418f8730db21e7
c steele <csteele@pandoradigital.com>
2023-12-11 07:40:36 -0800

    v1.0.0

--- SprinklerSchedule/SprinklerSchedule_child.groovy
+++ SprinklerSchedule/SprinklerSchedule_child.groovy
@@ -1,46 +1,56 @@
 /* 
 =============================================================================
 Hubitat Elevation Application
-Sprinkler scheduler (child application)
+Sprinkler Schedule (child application)
 
-    https://github.com/matt-hammond-001/hubitat-code
+    Original: Lighting Schedules https://github.com/matt-hammond-001/hubitat-code
+    This fork: Sprinkler Schedules https://github.com/csteele-PD/Hubitat-public/tree/master/SprinklerSchedule
 
 -----------------------------------------------------------------------------
 This code is licensed as follows:
 
-BSD 3-Clause License
+	BSD 3-Clause License
+	
+	Copyright (c) 2020, Matt Hammond
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+	
+	3. Neither the name of the copyright holder nor the names of its
+	   contributors may be used to endorse or promote products derived from
+	   this software without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	-----------------------------------------------------------------------------
+ *
+ *
+ *
+ * csteele: v1.0.0		Converted from Matt Hammond's Lighting Schedule (child)
+ *                	 Converted to capability.valve from switch 
+ *
+ */
+ 
+	public static String version()      {  return "v1.0.0"  }
 
-Copyright (c) 2020, Matt Hammond
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
------------------------------------------------------------------------------
-*/
-
-import groovy.transform.Field
+	import groovy.transform.Field
 
 definition(
 	name: "Sprinkler Schedule",
@@ -58,7 +68,8 @@ preferences {
 }
 
 def main(){
-	dynamicPage(name: "main", title: "Sprinkler Schedule", uninstall: true, install: true){
+	dynamicPage(name: "main", title: "", uninstall: true, install: true){
+	  displayHeader()
         section("<b>General</b>") {
             input "appLabel",
                 "text",
@@ -163,8 +174,8 @@ def main(){
                 </ul></i></div>\
                 """.stripIndent()
             
-            valves.each {
-                dev ->
+            valves.sort { it.label }.each {
+                 dev ->
                     def fieldName = "devTimings-$dev.id"
                     def info = ""
                     def value = settings[fieldName]
@@ -224,18 +235,18 @@ Regex for matching time period patterns
 
 Matches: TIMESPEC-TIMESPEC REMAINDER
 
-TIMESPEC = any of:
-             HHMM
-             HH:MM
-             sunrise
-             sunset
-             sunrise[+-]OFFSET
-             sunset[+-]OFFSET
-HH     := [0-2][0-9]
-MM     := [0-5][0-9]
-OFFSET := 0|(?:[1-9][0-9]{0,2})    .... between 1 and 3 digits, always starting with a non zero, or just zero
+	TIMESPEC = any of:
+	             HHMM
+	             HH:MM
+	             sunrise
+	             sunset
+	             sunrise[+-]OFFSET
+	             sunset[+-]OFFSET
+	HH     := [0-2][0-9]
+	MM     := [0-5][0-9]
+	OFFSET := 0|(?:[1-9][0-9]{0,2})    .... between 1 and 3 digits, always starting with a non zero, or just zero
 
-restricting format of OFFSET in this way means it will never match the 4 digit pattern or HHMM or HH:MM ... hopefully
+  restricting format of OFFSET in this way means it will never match the 4 digit pattern or HHMM or HH:MM ... hopefully
 */
 
 @Field static def timeRangePattern = ~/^[;, ]*(?:(?:([0-2][0-9])[:.]?([0-5][0-9]))|(?:(sunrise|sunset)( *[+-] *(?:0|(?:[1-9][0-9]{0,2})))?)) *(?:-|to) *(?:(?:([0-2][0-9])[:.]?([0-5][0-9]))|(?:(sunrise|sunset)( *[+-] *(?:0|(?:[1-9][0-9]{0,2})))?))[;, ]*(.*)$/
@@ -641,3 +652,17 @@ def calcTime(today, hrs, mins, relTo, offsetMins) {
 
     return [t, type, ref_t]
 }
+
+def getFormat(type, myText=""){            // Modified from @Stephack Code
+	if(type == "line") return "<hr style='background-color:#1A77C9; height: 1px; border: 0;'>"
+	if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
+}
+
+def displayHeader() {
+	section (getFormat("title", "Sprinkler Schedule")) {
+		paragraph "<div style='color:#1A77C9;text-align:right;font-weight:small;font-size:9px;'>Developed by: Matt Hammond, C Steele<br/>Current Version: ${version()} -  ${thisCopyright}</div>"
+		paragraph "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	}
+}
+
+def getThisCopyright(){"&copy; 2023 C Steele"}
